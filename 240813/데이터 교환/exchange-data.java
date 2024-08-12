b6f6df06ebeb462e750a157fde57846cc9251e1c
JoYeongMu <fprh13@gmail.com>
2024-08-13 00:12:19 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240813/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp = 0;
+        
+        temp = b;
+        b = a;
+        a = c;
+        c = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(c);
+
+
+    }
+}
