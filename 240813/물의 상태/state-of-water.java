34554148b44e56963619cf3858813ac580189613
JoYeongMu <fprh13@gmail.com>
2024-08-13 12:09:59 +0900

    Title: 물의 상태, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240813/물의 상태/state-of-water.java
+++ 240813/물의 상태/state-of-water.java
@@ -8,7 +8,7 @@ public class Main {
 
         if (temperature >= 100) {
             System.out.print("vapor");
-        } else if (0 < temperature) {
+        } else if (0 =< temperature) {
             System.out.print("water");
         } else {
             System.out.print("ice");
